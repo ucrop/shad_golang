7e57f10e66563a272d5d7cb4959bca4975634bb9
Sokurov David <sheridansiktop@gmail.com>
2020-12-25 19:08:15 +0300

    anime

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -1,7 +1,35 @@
-// +build !solution
-
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
+func main() {
+	argsWithProg := os.Args
+	argsWithProg = argsWithProg[1:]
+
+	for _, url := range argsWithProg {
+		err := DownloadFile(url)
+		if err != nil {
+			fmt.Println("The requested URL was not found.")
+			os.Exit(1)
+		}
+	}
+}
+
+func DownloadFile(url string) (err error) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	// check err
+	fmt.Printf("%s\n", b)
+	return err
 }
