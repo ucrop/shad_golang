c6d8e3491afaebc216d07c9fca63b2e4d5a52c44
Sokurov David <sheridansiktop@gmail.com>
2021-01-04 12:03:23 +0300

    solve keylock

--- keylock/keylock.go
+++ keylock/keylock.go
@@ -2,12 +2,60 @@
 
 package keylock
 
-type KeyLock struct{}
+// KeyLock ...
+type KeyLock struct {
+	mt   chan struct{}
+	used map[string]chan struct{}
+}
 
 func New() *KeyLock {
-	panic("implement me")
+	res := &KeyLock{
+		mt:   make(chan struct{}, 1),
+		used: make(map[string]chan struct{}),
+	}
+	res.mt <- struct{}{}
+	return res
 }
 
 func (l *KeyLock) LockKeys(keys []string, cancel <-chan struct{}) (canceled bool, unlock func()) {
-	panic("implement me")
+	for {
+		select {
+		case <-l.mt:
+		case <-cancel:
+			return true, nil
+		}
+
+		again := false
+	Loop:
+		for i, key := range keys {
+			_, ok := l.used[key]
+			if !ok {
+				l.used[key] = make(chan struct{}, 1)
+				l.used[key] <- struct{}{}
+			}
+
+			select {
+			case <-l.used[key]:
+			default:
+				for j := 0; j < i; j++ {
+					l.used[keys[j]] <- struct{}{}
+				}
+				l.mt <- struct{}{}
+				again = true
+				break Loop
+			}
+		}
+
+		if again {
+			continue
+		}
+
+		l.mt <- struct{}{}
+
+		return false, func() {
+			for _, key := range keys {
+				l.used[key] <- struct{}{}
+			}
+		}
+	}
 }
